08cde962a5338f7662196d84269e14e25b61ded4
GameON001-source <memesegames.acsa321@gmail.com>
2026-05-07 13:51:28 -0300

    Create instalador.bat

---
+++ instalador.bat
@@ -0,0 +1,19 @@
+@echo off
+title GameOn Digital - Instalador
+color 0A
+
+cls
+echo ============================================
+echo        GAMEON DIGITAL - INICIANDO...
+echo ============================================
+echo.
+echo [OK] Conectado ao servidor.
+echo Aguarde enquanto os arquivos sao preparados...
+echo.
+
+:: AQUI ABAIXO VOCÊ COLOCARÁ OS COMANDOS DE DOWNLOAD NO FUTURO
+:: Exemplo: curl -L "link_do_jogo" -o jogo.zip
+
+echo Instalacao concluida!
+pause
+exit
