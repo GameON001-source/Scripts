b3cacda22b505659d2d0a759901c355017ccd377
GameON001-source <memesegames.acsa321@gmail.com>
2026-05-07 14:09:40 -0300

    Update instalador.bat

--- instalador.bat
+++ instalador.bat
@@ -1,19 +1,34 @@
 @echo off
-title GameOn Digital - Instalador
+title GameOn Digital - Instalador Automatico
 color 0A
 
 cls
 echo ============================================
-echo        GAMEON DIGITAL - INICIANDO...
+echo        GAMEON DIGITAL - INSTALADOR
 echo ============================================
 echo.
-echo [OK] Conectado ao servidor.
-echo Aguarde enquanto os arquivos sao preparados...
+echo [1/3] Baixando biblioteca de jogos (1.5GB)...
+echo Aguarde, o tempo depende da sua internet.
 echo.
 
-:: AQUI ABAIXO VOCÊ COLOCARÁ OS COMANDOS DE DOWNLOAD NO FUTURO
-:: Exemplo: curl -L "link_do_jogo" -o jogo.zip
+:: O comando abaixo baixa o arquivo direto do seu MediaFire
+curl -L "https://www.mediafire.com/file/baoqw8uen6qnh83/JOGOS_STEAM_GameON.zip/file" -o JOGOS_GameON.zip
 
-echo Instalacao concluida!
+echo.
+echo [2/3] Download concluido! Extraindo arquivos...
+echo.
+
+:: O tar extrai o ZIP automaticamente para uma pasta chamada JOGOS_GameON
+tar -xf JOGOS_GameON.zip
+
+echo [3/3] Limpando arquivos temporarios...
+del JOGOS_GameON.zip
+
+echo.
+echo ============================================
+echo      INSTALACAO CONCLUIDA COM SUCESSO!
+echo    Os jogos estao na pasta JOGOS_STEAM
+echo ============================================
+echo.
 pause
 exit
