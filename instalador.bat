190d37b4bcfabb44acd84a51959b382db5697031
GameON001-source <memesegames.acsa321@gmail.com>
2026-05-07 14:24:33 -0300

    Update instalador.bat

--- instalador.bat
+++ instalador.bat
@@ -8,9 +8,9 @@ echo ============================================
 echo           GAMEON DIGITAL - MENU
 echo ============================================
 echo.
-echo  [1] INSTALAR BIBLIOTECA (1.05 GB)
+echo  [1] INSTALAR BIBLIOTECA (1.5GB)
 echo  [2] ATUALIZAR ARQUIVOS
-echo  [3] DESINSTALAR (REMOVER TUDO)
+echo  [3] DESINSTALAR TUDO
 echo  [4] SAIR
 echo.
 echo ============================================
@@ -24,50 +24,54 @@ goto :menu
 
 :instalar
 cls
-echo [!] Iniciando Instalacao...
-echo [!] Baixando 1.05 GB... Isso pode demorar alguns minutos.
-echo.
-:: Usando o seu link direto que voce enviou
-curl -L -o JOGOS_GameON.zip "https://download1474.mediafire.com/bfzl5bfosxxgqc7qK9mpuyDwxSsQxtRcxHJYKu5GvHs90NO5Faw8On3RpQLcEOsdIwxG1K2rU34tYbcN8XrSajJyobUiEEqbhXuKakS6NvMesrIPzvNvrQyk3mN9RzMqlx_sXGzLbqlLw8cD9dLnHtljnvVEiGRkcZTLeRUxw1wO7g/baoqw8uen6qnh83/JOGOS+STEAM+GameON.zip"
+echo [!] Criando pasta GameON no C:\...
+if not exist "C:\GameON" mkdir "C:\GameON"
+cd /d "C:\GameON"
 
-if not exist JOGOS_GameON.zip (
+echo [!] Iniciando Download (1.5GB)...
+echo Isso pode demorar varios minutos dependendo da sua internet.
+echo.
+:: Usando o seu Link Direto atualizado
+curl -L "https://download1474.mediafire.com/bfzl5bfosxxgqc7qK9mpuyDwxSsQxtRcxHJYKu5GvHs90NO5Faw8On3RpQLcEOsdIwxG1K2rU34tYbcN8XrSajJyobUiEEqbhXuKakS6NvMesrIPzvNvrQyk3mN9RzMqlx_sXGzLbqlLw8cD9dLnHtljnvVEiGRkcZTLeRUxw1wO7g/baoqw8uen6qnh83/JOGOS+STEAM+GameON.zip" -o JOGOS_GameON.zip
+
+if %errorlevel% neq 0 (
     echo.
-    echo [ERRO] O download falhou. Verifique sua internet.
+    echo [ERRO] O link de download expirou! 
+    echo Por favor, contate o suporte GameOn.
     pause
     goto :menu
 )
 
 echo.
-echo [!] Extraindo jogos...
+echo [!] Download concluido! Extraindo jogos...
 tar -xf JOGOS_GameON.zip
 del JOGOS_GameON.zip
 echo.
-echo [+] Instalacao concluida com sucesso!
-echo Os arquivos estao na pasta onde voce executou este comando.
+echo [+] Tudo pronto! Seus jogos estao em C:\GameON
 pause
 goto :menu
 
 :atualizar
 cls
+cd /d "C:\GameON"
 echo [!] Verificando atualizacoes...
-curl -L -o JOGOS_GameON.zip "https://download1474.mediafire.com/bfzl5bfosxxgqc7qK9mpuyDwxSsQxtRcxHJYKu5GvHs90NO5Faw8On3RpQLcEOsdIwxG1K2rU34tYbcN8XrSajJyobUiEEqbhXuKakS6NvMesrIPzvNvrQyk3mN9RzMqlx_sXGzLbqlLw8cD9dLnHtljnvVEiGRkcZTLeRUxw1wO7g/baoqw8uen6qnh83/JOGOS+STEAM+GameON.zip"
+curl -L "https://download1474.mediafire.com/bfzl5bfosxxgqc7qK9mpuyDwxSsQxtRcxHJYKu5GvHs90NO5Faw8On3RpQLcEOsdIwxG1K2rU34tYbcN8XrSajJyobUiEEqbhXuKakS6NvMesrIPzvNvrQyk3mN9RzMqlx_sXGzLbqlLw8cD9dLnHtljnvVEiGRkcZTLeRUxw1wO7g/baoqw8uen6qnh83/JOGOS+STEAM+GameON.zip" -o JOGOS_GameON.zip
 tar -xf JOGOS_GameON.zip
 del JOGOS_GameON.zip
 echo.
-echo [+] Atualizado com sucesso!
+echo [+] Arquivos atualizados com sucesso!
 pause
 goto :menu
 
 :desinstalar
 cls
-echo [!] ATENCAO: Isso vai apagar a pasta de jogos.
-set /p confirma="Tem certeza que deseja apagar? (S/N): "
+echo [!] ATENCAO: Todos os jogos em C:\GameON serao apagados.
+set /p confirma="Deseja continuar? (S/N): "
 if /i "%confirma%"=="S" (
-    :: O comando abaixo apaga a pasta que o seu ZIP cria. 
-    :: Ajuste o nome "JOGOS STEAM GameON" se a pasta tiver outro nome.
-    rd /s /q "JOGOS STEAM GameON" 2>nul
+    cd /d C:\
+    rd /s /q "C:\GameON"
     echo.
-    echo [-] Arquivos removidos.
+    echo [-] Pasta removida com sucesso.
 )
 pause
 goto :menu
