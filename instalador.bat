128f515462d5c2ad0aec68654dd6008afccde0bb
GameON001-source <memesegames.acsa321@gmail.com>
2026-05-08 00:14:20 -0300

    Update instalador.bat

--- instalador.bat
+++ instalador.bat
@@ -1,5 +1,6 @@
 # GameOn Digital - Oficial
 $senhaCorreta = "2727"
+
 Clear-Host
 Write-Host "============================================" -ForegroundColor Cyan
 Write-Host "          GAMEON DIGITAL - ACESSO" -ForegroundColor Cyan
@@ -7,7 +8,7 @@ Write-Host "============================================" -ForegroundColor Cyan
 $tentativa = Read-Host " Digite sua Chave"
 
 if ($tentativa -ne $senhaCorreta) {
-    Write-Host "[ERRO] Chave incorreta!" -ForegroundColor Red
+    Write-Host "`n[ERRO] Chave incorreta!" -ForegroundColor Red
     Start-Sleep -Seconds 2; exit
 }
 
@@ -15,11 +16,12 @@ Write-Host "`n[!] Preparando ambiente..." -ForegroundColor Yellow
 if (!(Test-Path "C:\GameON")) { New-Item -Path "C:\GameON" -ItemType Directory | Out-Null }
 Set-Location "C:\GameON"
 
-Write-Host "[!] Baixando arquivos... Aguarde." -ForegroundColor Cyan
-$url = "https://drive.google.com/uc?export=download&id=17_OBFcod8dKv6rXhg8_T2gfkohYZ8hx-"
+Write-Host "[!] Baixando jogos... Aguarde." -ForegroundColor Cyan
+# Link direto para download
+$url = "https://docs.google.com/uc?export=download&id=17_OBFcod8dKv6rXhg8_T2gfkohYZ8hx-&confirm=t"
 Start-BitsTransfer -Source $url -Destination "C:\GameON\JOGOS.zip"
 
-Write-Host "[!] Extraindo jogos..." -ForegroundColor Yellow
+Write-Host "[!] Extraindo arquivos..." -ForegroundColor Yellow
 Expand-Archive -Path "C:\GameON\JOGOS.zip" -DestinationPath "C:\GameON" -Force
 Remove-Item "C:\GameON\JOGOS.zip" -Force
 
