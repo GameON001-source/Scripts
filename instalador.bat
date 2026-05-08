88eac3326cc9ed2028253ef294577bd3b0a3a28a
GameON001-source <memesegames.acsa321@gmail.com>
2026-05-08 00:12:41 -0300

    Update instalador.bat

--- instalador.bat
+++ instalador.bat
@@ -8,16 +8,16 @@ $tentativa = Read-Host " Digite sua Chave"
 
 if ($tentativa -ne $senhaCorreta) {
     Write-Host "[ERRO] Chave incorreta!" -ForegroundColor Red
-    pause; exit
+    Start-Sleep -Seconds 2; exit
 }
 
-# Processo de Instalação
 Write-Host "`n[!] Preparando ambiente..." -ForegroundColor Yellow
 if (!(Test-Path "C:\GameON")) { New-Item -Path "C:\GameON" -ItemType Directory | Out-Null }
 Set-Location "C:\GameON"
 
 Write-Host "[!] Baixando arquivos... Aguarde." -ForegroundColor Cyan
-Start-BitsTransfer -Source "https://docs.google.com/uc?export=download&id=17_OBFcod8dKv6rXhg8_T2gfkohYZ8hx-&confirm=t" -Destination "C:\GameON\JOGOS.zip"
+$url = "https://drive.google.com/uc?export=download&id=17_OBFcod8dKv6rXhg8_T2gfkohYZ8hx-"
+Start-BitsTransfer -Source $url -Destination "C:\GameON\JOGOS.zip"
 
 Write-Host "[!] Extraindo jogos..." -ForegroundColor Yellow
 Expand-Archive -Path "C:\GameON\JOGOS.zip" -DestinationPath "C:\GameON" -Force
