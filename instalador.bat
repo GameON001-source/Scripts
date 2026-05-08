b80a976a4027a8a4f7dcae5b8dc303c963c24c92
GameON001-source <memesegames.acsa321@gmail.com>
2026-05-08 00:31:14 -0300

    Update instalador.bat

--- instalador.bat
+++ instalador.bat
@@ -2,7 +2,7 @@
 title GameOn Digital - Central de Jogos
 color 0A
 
-:: CONFIGURAÇÃO DA SENHA
+:: SENHA
 set "senhaCorreta=2727"
 
 :autenticacao
@@ -47,26 +47,16 @@ cls
 echo [!] Preparando ambiente...
 if not exist "C:\GameON" mkdir "C:\GameON"
 cd /d "C:\GameON"
-
-echo [!] Baixando arquivos... Aguarde (Isso pode demorar).
+echo [!] Baixando arquivos... Aguarde.
 echo.
-:: O segredo: curl com -L e &confirm=t entre aspas para o Google nao bloquear
+:: Link direto com confirmacao para o Google Drive
 curl -L "https://docs.google.com/uc?export=download&id=17_OBFcod8dKv6rXhg8_T2gfkohYZ8hx-&confirm=t" -o "JOGOS_GameON.zip"
-
-if not exist "JOGOS_GameON.zip" (
-    echo.
-    echo [ERRO] Falha no download.
-    pause
-    goto :menu
-)
-
 echo.
-echo [!] Instalando e extraindo arquivos...
+echo [!] Instalando...
 tar -xf "JOGOS_GameON.zip"
 del /f /q "JOGOS_GameON.zip"
-
 echo.
-echo [+] INSTALACAO CONCLUIDA!
+echo [+] CONCLUIDO!
 pause
 goto :menu
 
@@ -88,7 +78,7 @@ set /p confirma="> "
 if /i "%confirma%"=="S" (
     cd /d C:\
     rd /s /q "C:\GameON"
-    echo [-] Pasta removida.
+    echo [-] Removido.
 )
 pause
 goto :menu
